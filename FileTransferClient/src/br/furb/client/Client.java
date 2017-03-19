cffaa2ed409da9168ffead3790bcb89d32fcd565
flaviolosada <usuario@192.168.25.224>
2017-03-19 18:57:03 -0300

    ajuste no envio de arquivo

--- FileTransferClient/src/br/furb/client/Client.java
+++ FileTransferClient/src/br/furb/client/Client.java
@@ -2,14 +2,19 @@ package br.furb.client;
 
 
 import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.Socket;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -39,21 +44,48 @@ public class Client extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(131, 131, 131)
+                .addComponent(jButton1)
+                .addContainerGap(196, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(101, 101, 101)
+                .addComponent(jButton1)
+                .addContainerGap(176, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        File file = openFile();
+        try {
+            sendMessageMulticast(file.getName());
+            Address serverAddress = receiveServerAddress();
+            sendFileSocket(serverAddress, file);
+            JOptionPane.showMessageDialog(this, "Envio concluído", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Erro no envio do arquivo", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -108,7 +140,7 @@ public class Client extends javax.swing.JFrame {
         }
     }
     
-    private static Address receiveServerName() throws Exception {
+    private static Address receiveServerAddress() throws Exception {
         byte[] buf = new byte[1024];
         DatagramPacket request = null;
         DatagramSocket socket = new DatagramSocket(5005);
@@ -136,14 +168,35 @@ public class Client extends javax.swing.JFrame {
         if (file != null ) {
             /* Inicializacao de socket TCP */
             socket = new Socket(serverAddress.getIpAddress(), serverAddress.getPortNumber());
-            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-            bis.read(sendData);
-            socket.getOutputStream().write(sendData);
-            bis.close();
+            
+            FileInputStream in = new FileInputStream(file);
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out);
+            BufferedWriter writer = new BufferedWriter(osw);
+            writer.write(file.getName() + "\n");
+            writer.flush();
+            int tamanho = 4096; // buffer de 4KB  
+	    byte[] buffer = new byte[tamanho];  
+	    int lidos = -1;  
+	    while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
+	        out.write(buffer, 0, lidos);  
+	    }  
+            
             socket.close();
         }
     }
+    
+    private static File openFile() {
+        JFileChooser openFile = new JFileChooser();
+        openFile.setAcceptAllFileFilterUsed(false);
+        openFile.setDialogType(JFileChooser.OPEN_DIALOG);
+        if (openFile.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            return openFile.getSelectedFile();
+        }
+        return null;
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
